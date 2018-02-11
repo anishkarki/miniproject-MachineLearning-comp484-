375f5c84007f04459baa06a693bd384020c6f7fe
anishkarki <anish.karki3579@gmail.com>
2018-02-11 16:20:30 +0545

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,134 @@
+import json, urllib2, time
+from datetime import datetime
+from time import mktime
+
+from Bpnnetwork import NeuralNetwork
+
+def normalizePrice(price, minimum, maximum):
+    return ((2*price - (maximum + minimum)) / (maximum - minimum))
+
+def denormalizePrice(price, minimum, maximum):
+    return (((price*(maximum-minimum))/2) + (maximum + minimum))/2
+
+def rollingWindow(seq, windowSize):
+    it = iter(seq)
+    win = [it.next() for cnt in xrange(windowSize)] # First window
+    yield win
+    for e in it: # Subsequent windows
+        win[:-1] = win[1:]
+        win[-1] = e
+        yield win
+
+def getMovingAverage(values, windowSize):
+    movingAverages = []
+    
+    for w in rollingWindow(values, windowSize):
+        movingAverages.append(sum(w)/len(w))
+
+    return movingAverages
+
+def getMinimums(values, windowSize):
+    minimums = []
+
+    for w in rollingWindow(values, windowSize):
+        minimums.append(min(w))
+            
+    return minimums
+
+def getMaximums(values, windowSize):
+    maximums = []
+
+    for w in rollingWindow(values, windowSize):
+        maximums.append(max(w))
+
+    return maximums
+
+def getTimeSeriesValues(values, window):
+    movingAverages = getMovingAverage(values, window)
+    minimums = getMinimums(values, window)
+    maximums = getMaximums(values, window)
+
+    returnData = []
+
+    # build items of the form [[average, minimum, maximum], normalized price]
+    for i in range(0, len(movingAverages)):
+        inputNode = [movingAverages[i], minimums[i], maximums[i]]
+        price = normalizePrice(values[len(movingAverages) - (i + 1)], minimums[i], maximums[i])
+        outputNode = [price]
+        tempItem = [inputNode, outputNode]
+        returnData.append(tempItem)
+
+    return returnData
+
+def getHistoricalData(stockSymbol):
+    historicalPrices = []
+    
+    # login to API
+    urllib2.urlopen("http://api.kibot.com/?action=login&user=guest&password=guest")
+
+    # get 14 days of data from API (business days only, could be < 10)
+    url = "http://api.kibot.com/?action=history&symbol=" + stockSymbol + "&interval=daily&period=14&unadjusted=1&regularsession=1"
+    apiData = urllib2.urlopen(url).read().split("\n")
+    for line in apiData:
+        if(len(line) > 0):
+            tempLine = line.split(',')
+            price = float(tempLine[1])
+            historicalPrices.append(price)
+
+    return historicalPrices
+
+def getTrainingData(stockSymbol):
+    historicalData = getHistoricalData(stockSymbol)
+
+    # reverse it so we're using the most recent data first, ensure we only have 9 data points
+    historicalData.reverse()
+    del historicalData[9:]
+
+    # get five 5-day moving averages, 5-day lows, and 5-day highs, associated with the closing price
+    trainingData = getTimeSeriesValues(historicalData, 5)
+
+    return trainingData
+
+def getPredictionData(stockSymbol):
+    historicalData = getHistoricalData(stockSymbol)
+
+    # reverse it so we're using the most recent data first, then ensure we only have 5 data points
+    historicalData.reverse()
+    del historicalData[5:]
+
+    # get five 5-day moving averages, 5-day lows, and 5-day highs
+    predictionData = getTimeSeriesValues(historicalData, 5)
+    # remove associated closing price
+    predictionData = predictionData[0][0]
+
+    return predictionData
+
+def analyzeSymbol(stockSymbol):
+    startTime = time.time()
+    
+    trainingData = getTrainingData(stockSymbol)
+    
+    network = NeuralNetwork(inputNodes = 3, hiddenNodes = 3, outputNodes = 1)
+
+    network.train(trainingData)
+
+    # get rolling data for most recent day
+    predictionData = getPredictionData(stockSymbol)
+
+    # get prediction
+    returnPrice = network.test(predictionData)
+
+    # de-normalize and return predicted stock price
+    predictedStockPrice = denormalizePrice(returnPrice, predictionData[1], predictionData[2])
+
+    # create return object, including the amount of time used to predict
+    returnData = {}
+    returnData['price'] = predictedStockPrice
+    returnData['time'] = time.time() - startTime
+
+    return returnData
+
+## ================================================================
+
+if __name__ == "__main__":
+    print analyzeSymbol("GOOG")
